7659794cf3dd347a065eb9533f24da04e0beed19
Yaroslava <yuriandreev1ch@gmail.com>
2023-04-12 17:43:54 +0300

    1

--- src/components/dashboard.jsx
+++ src/components/dashboard.jsx
@@ -1,10 +1,38 @@
-import React from "react";
+
+import Draggable from "react-draggable";
+import React, { useState } from "react";
+
 
 function dashboard() {
-    
+
+  const [izv, setIzv] = useState([
+
+  ])
+ const [activeDrags, setactiveDrags] = useState(0)
+  const startMove = () =>{}
+  const endMove = () =>{}
+     const onStart = () => {
+      setactiveDrags(activeDrags + 1)
+    };
+  
+  const  onStop = () => {
+      setactiveDrags(activeDrags - 1)
+      
+    };
+    const dragHandlers = {onStart: onStart, onStop: onStop};
+
+  //  const onDrop = (e) => {
+  //     setactiveDrags(activeDrags - 1)
+  //     if (e.target.classList.contains("drop-target")) {
+  //       alert("Dropped!");
+  //       e.target.classList.remove('hovered');
+  //     }
+  //   };
   return (
     <div className='flex'>
-      <div>1</div>
+      <div ><Draggable onStart={() => false} 
+      {...dragHandlers}
+      >1</Draggable></div>
       <div>2</div>
       <div>3</div>
     </div>
